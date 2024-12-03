94a7b089ce062b849e0b1ed3b8ec4619b15ad0ea
Marc Burri <marc.burri91@gmail.com>
2024-12-03 13:02:06 +0100

    backward compatibility

--- R/bridgr-package.R
+++ R/bridgr-package.R
@@ -5,11 +5,19 @@
 "_PACKAGE"
 utils::globalVariables(c(".", "id", "ind_freq", "month", "week", "n_rows", "quarter", "time", "year"))
 
-## usethis namespace: start
 #' @importFrom magrittr %>%
 #' @importFrom rlang %||%
 #' @importFrom lubridate %m+% %m-%
 #' @importFrom stats as.formula frequency lm na.omit
 #' @importFrom utils tail
-## usethis namespace: end
+#' @importFrom generics forecast
+NULL
+
+#' Generics to re-export
+
+#' @export
+generics::forecast
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
